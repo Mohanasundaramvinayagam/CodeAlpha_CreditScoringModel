18600b1ccd0010c219a0f0dd0ee62c574fc8431a
Mohanasundaram V <mohanasundaramvinayagam@gmail.com>
2025-07-01 23:00:11 +0530

    removing unwanted commands

--- credit (1).py
+++ credit (1).py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,68 +12,42 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, roc_curve
 from sklearn.metrics import confusion_matrix
 
-# E
-
-
-
-# In[2]:
-
-
 data = pd.read_csv('credit_data.csv')
 print(data.head())
 print(data.info())
 
-
-# In[3]:
-
-
 # Check for null values
 print(data.isnull().sum())
 
-# Drop or fill nulls
+#  nulls
 data.fillna(data.mean(), inplace=True)
 
 # Separate features and target
 X = data[['income', 'debt', 'payment_history']]
 y = data['credit_score']
 
-# Feature scaling (important for Logistic Regression)
+# Feature scaling 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
-
-
-# In[4]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42)
 
 
-# In[5]:
-
 
 log_model = LogisticRegression()
 log_model.fit(X_train, y_train)
 y_pred_log = log_model.predict(X_test)
-
-
-# In[6]:
-
 
 tree_model = DecisionTreeClassifier()
 tree_model.fit(X_train, y_train)
 y_pred_tree = tree_model.predict(X_test)
 
 
-# In[7]:
-
-
 rf_model = RandomForestClassifier(n_estimators=100)
 rf_model.fit(X_train, y_train)
 y_pred_rf = rf_model.predict(X_test)
-
-
-# In[8]:
 
 
 def evaluate_model(y_test, y_pred, model_name):
@@ -88,21 +56,9 @@ def evaluate_model(y_test, y_pred, model_name):
     print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 
 
-# In[9]:
-
-
 evaluate_model(y_test, y_pred_log, "Logistic Regression")
 evaluate_model(y_test, y_pred_tree, "Decision Tree")
 evaluate_model(y_test, y_pred_rf, "Random Forest")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 log_prob = log_model.predict_proba(X_test)[:, 1]  # probability of class 1 (creditworthy)
@@ -120,13 +76,6 @@ plt.legend()
 plt.show()
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
